6262fcea990e59d916397f40bd5c090029f450cb
Kris <kamienieckik@gmail.com>
2020-12-15 00:25:01 +0100

    Classnames lib added

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import classNames from 'classnames'
 
-export default function ProgressBar({ className, trackRemaining = false, procent = 10 }) {
+export default function ProgressBar({ className, trackRemaining = false, procent = 10, big = false }) {
   const padding = trackRemaining ? 'paddingLeft' : 'paddingRight';
+  let classes = classNames(
+    "progress",
+    className,
+    {
+      "progress--big": big,
+    }
+  )
 
-  return (<div className={"progress progress--big" + className} style={{ [padding]: `${100 - procent}%` }} ></div>)
+  return (<div className={classes} style={{ [padding]: `${100 - procent}%` }} ></div>)
 }
