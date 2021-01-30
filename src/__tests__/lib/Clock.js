c69b15be1fe6eca032f546a20c70347fd49b1285
Kris <kamienieckik@gmail.com>
2021-01-30 22:13:18 +0100

    Clock tests with renderer

--- src/__tests__/lib/Clock.js
+++ src/__tests__/lib/Clock.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import Clock from '../../components/Clock'
+import Clock from '../../components/Clock';
+import ReactDOM from 'react-dom';
 
 
-describe('', () => {
-    it('', () => {
-        expect(<Clock />).toEqual();
+describe('<Clock> component', () => {
+    let root;
+    beforeEach(() => {
+        root = document.createElement("div");
+        ReactDOM.render(<Clock minutes={10} seconds={20} />, root)
+    })
+
+    it('renders properly H2 element', () => {
+        expect(root.childNodes[0].nodeName).toEqual("H2")
+    });
+    it('renders properly with className', () => {
+        expect(root.childNodes[0].className).toEqual("clock")
+    });
+    it('renders properly with text', () => {
+        expect(root.childNodes[0].textContent).toEqual("Pozostało 00:10:20.000")
+    });
+    it('sets ', () => {
+        expect(<Clock />).toEqual(<Clock hours={0} miliseconds={0} minutes={20} seconds={0} />);
     });
 })
 
